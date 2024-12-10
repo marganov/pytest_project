6f2b94c060a58a50ad9671cc381a0391350a61a2
Alexei Marganov <marganovalexey@gmail.com>
2024-12-10 12:45:56 +0300

    вынес локатор в отдельный файл

--- test_main_page.py
+++ test_main_page.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
 
-link = "http://selenium1py.pythonanywhere.com/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
 
 
 # Было так:
@@ -27,15 +27,14 @@ def test_guest_can_go_to_login_page(browser):
 import pytest
 from .pages.main_page import MainPage
 
+link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
 def test_should_be_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
