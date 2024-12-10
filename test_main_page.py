6da45fd92fac241e5627291e13220c2bb5c560c4
Alexei Marganov <marganovalexey@gmail.com>
2024-12-10 09:34:04 +0300

    тест наполовину failed

--- test_main_page.py
+++ test_main_page.py
@@ -31,3 +31,9 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+
+def test_should_be_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page(open)
+    page.sould_be_login_link()
